65d31f02bc442fc1179200f292e9aea6eca98015
DMITRY <manujlv@mail.ru>
2015-11-21 15:42:41 +0300

    first commit

---
+++ 2lab/Checkers/CheckersGeneralClasses/src/main/java/stasuses/Status.java
@@ -0,0 +1,9 @@
+package stasuses;
+
+/**
+ * Created by Дмитрий on 04.11.2015.
+ */
+public enum Status {
+    WAITING,
+    GAMING
+}
---
+++ 2lab/Checkers/CheckersGeneralClasses/src/main/java/stasuses/TypeChecker.java
@@ -0,0 +1,8 @@
+package stasuses;
+
+/**
+ * Created by Дмитрий on 10.11.2015.
+ */
+public enum TypeChecker{
+    WHITE, BLACK, KING
+}
